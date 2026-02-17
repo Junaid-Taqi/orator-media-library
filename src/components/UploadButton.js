b53249da3a3032b2cad5b8cdb8215d532a610f5a
Muhammad Junaid <m.junaid8088@gmail.com>
2026-02-17 08:09:47 +0500

    inital commit

--- src/components/UploadButton.js
+++ src/components/UploadButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons';
+import { faUpload } from '@fortawesome/free-solid-svg-icons';
 
 export default function UploadButton() {
   return (
     <button className="upload-btn">
-      <FontAwesomeIcon icon={faCloudUploadAlt} />
+      <FontAwesomeIcon icon={faUpload} />
       <span>Upload Media</span>
     </button>
   );
